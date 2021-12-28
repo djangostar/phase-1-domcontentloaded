1a31ab21f17a731a06322b13fb0f9c5965d35c34
Juan-Sergio Khoshdel <jsergiofm85@gmail.com>
2021-12-27 19:59:57 -0800

    commit tree 019cea94b9f91eef0015918d0b16054d61be158c

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', () =>
+   document.getElementById('text').textContent = 'This is really cool!')
+
+//With variable
+
+//document.addEventListener('DOMContentLoaded', function() {
+//    const text = document.getElementById('text')
+//    text.textContent = 'This is really cool!'
+//})
